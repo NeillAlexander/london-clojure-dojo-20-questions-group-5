c2e0cdd28e9c9f7794f90ba58b3ae0735e0aa46c
Neill Alexander <neillalexander@gmail.com>
2011-09-27 20:11:40 +0100

    next

--- src/questions/core.clj
+++ src/questions/core.clj
@@ -4,4 +4,7 @@
 (def url "http://172.6.1.227:8080/20-questions/latest")
 
 (defn get-data [url]
-  (:body (client/get url)))
+  (read-string (:body (client/get url))))
+
+(defn play-game [{:keys [ question yes no]} ]
+  (println question ))
