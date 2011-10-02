6acaeb9901c129b1a38bda682a20c730ec161023
Neill Alexander <neillalexander@gmail.com>
2011-10-02 17:44:23 +0100

    Added functioning prompt and game loop

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject questions "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [clj-http "0.2.1"]])
+                 [clj-http "0.2.1"]]
+  :dev-dependencies [[swank-clojure/swank-clojure "1.3.2"]]
+  :main questions.core)
--- src/questions/core.clj
+++ src/questions/core.clj
@@ -1,33 +1,83 @@
 (ns questions.core
   (:require [clj-http.client :as client]
-            ))
+            [swank.swank :as swank]
+            [clojure.string :as str]))
 
+;; For the online version we did at the dojo
+;; See https://github.com/ppotter/questions-server
+;; The server didn't run when I cloned it locally so I've only done the work
+;; to make the game work locally.
 (def url "http://172.6.1.227:8080/20-questions/latest")
 
 (defn get-data [url]
   (read-string (:body (client/get url))))
 
-(defn ask-question [data]
-  (:question data))
+;; This will gradually grow through the lifetime of the game
+(def starting-data {:question "Are you alive?", :yes "Trevor McDonald", :no "Elizabeth I"})
 
-(defn respond-to-question [data answer]
-  (data answer)) 
+(def transform-response {"y", :yes, "yes" :yes, "n" :no, "no" :no})
+
+(def validate-response (set (keys transform-response)))
+
+(defn make-game-data [questions]
+  {:questions questions, :responses []})
 
 (defn is-question? [answer]
   (map? answer))
 
-(defn is-person? [answer]
-  (not (is-question? answer)))
+(defn is-valid-game [data]
+  (and (map? data) (:questions data)))
 
-(defn make-question [question new-options]
-  (merge {:question question} new-options))
+(defn prompt!
+  "If supplied, valid is passed to check the input to make sure it is valid."
+  ([s]
+     (prompt! s str/blank?))
+  ([s validate transform]
+     (println s)
+     (flush)
+     (let [input (str/trim (read-line))]
+       (if (not (validate input))
+         (recur s validate transform)
+         (transform input)))))
 
-(defn suggest-new-question [data answer question alt-key alt]
-  (let [previous (data answer)]
-    (assoc data answer (make-question question {alt-key alt answer previous}))))
+(defn prompt-response! [s]
+  (prompt! s validate-response transform-response))
 
-(comment
-  (defn play-game [{:keys [ question yes no]} ]
-    (println question )
-    (let [input (keyword (read-line))]
-      (if (= :yes input)) )))
+(defn next-question [{:keys [questions responses]}]
+  (if (seq responses)
+    (:question (get-in questions responses))
+    (:question questions)))
+
+(defn current-response [{:keys [questions responses]}]
+  (get-in questions responses))
+
+(defn add-response [data response]
+  (update-in data [:responses] conj response))
+
+(defn wipe-responses [data]
+  (assoc-in data [:responses] []))
+
+(defn ask-question
+  "Prints the question and prompts for a response, returning :yes or :no"
+  [data]
+  (prompt-response! (next-question data)))
+
+(defn guess [data]
+  (let [response (prompt-response! (str "Are you " (current-response data) "?"))]
+    (if (= :yes response)
+      (println "Yay! I win!!")
+      data)))
+
+(defn play
+  [data]
+  (let [data (add-response data (ask-question data))]
+    (if (is-question? (current-response data))
+      (recur data)
+      (guess data))))
+
+(defn -main [& [swank? :as args]]
+  (if swank? (swank/start-repl 4005))
+  (loop [data (make-game-data starting-data)]
+    (if (is-valid-game data)
+      (recur (wipe-responses (play data)))
+      (System/exit 1))))
